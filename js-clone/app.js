42adb1d682d7e2ecb1c04e08f57a0bf074a889f3
seung_ho_choi.s <chltmdgh517@naver.com>
2023-06-26 09:56:57 +0900

    boolean, let, null, undefined

--- js-clone/app.js
+++ js-clone/app.js
@@ -4,6 +4,18 @@ console.log(1234156);
 const a=5;
 const b=2;
 const myName="승호";
+let myName1="승호2";
+
+const amIFat2=false;
+const amIFat1=true;
+let something;
+
+const abc=null;
+console.log(abc);
+
+
+console.log(something);
+
 
 console.log(a%b);
 console.log(a+b);
